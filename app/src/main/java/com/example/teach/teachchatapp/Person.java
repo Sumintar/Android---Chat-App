bbd23e29a022b77ea05cd02603d11ca8831f1670
TeacHKal <teach.inbox@gmail.com>
2016-05-17 15:58:11 +0200

    Test

---
+++ app/src/main/java/com/example/teach/teachchatapp/Person.java
@@ -0,0 +1,34 @@
+package com.example.teach.teachchatapp;
+
+/**
+ * Created by TeacH on 5/16/2016.
+ */
+public class Person {
+
+    private String name = null;
+    private String message = null;
+
+    public Person(String name, String message)
+    {
+        this.name = name;
+        this.message = message;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
